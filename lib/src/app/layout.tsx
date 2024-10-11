ca9de3f4fe65520e8750aebf0c55e0d072c4d039
Legit-Sam <webdeveloper2167@gmail.com>
2024-10-11 01:00:12 +0100

    update

--- lib/src/app/layout.tsx
+++ lib/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import  ConvexClientProvider  from "./ConvexClientProvider";
-import { ThemeProvider } from "@/providers/theme-provider";
+import { ThemeProvider } from "@/components/home/theme-provider";
 
 
 const inter = Inter({ subsets: ["latin"] });
