45927506131cd6659fe7e5bf07d85cee9366401c
Legit-Sam <webdeveloper2167@gmail.com>
2024-10-12 06:19:26 +0100

    updates

--- components/home/media-dropdown.tsx
+++ components/home/media-dropdown.tsx
@@ -1,206 +1,165 @@
 import { useEffect, useRef, useState } from "react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 import { ImageIcon, Plus, Video } from "lucide-react";
 import { Dialog, DialogContent, DialogDescription } from "../ui/dialog";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import ReactPlayer from "react-player";
-// import toast from "react-hot-toast";
 import { useMutation, useQuery } from "convex/react";
 import { useConversationStore } from "@/app/store/chatStore";
 import { api } from "@/convex/_generated/api";
 
 const MediaDropdown = () => {
-	const imageInput = useRef<HTMLInputElement>(null);
-	const videoInput = useRef<HTMLInputElement>(null);
-	const [selectedImage, setSelectedImage] = useState<File | null>(null);
-	const [selectedVideo, setSelectedVideo] = useState<File | null>(null);
+  const imageInput = useRef<HTMLInputElement>(null);
+  const videoInput = useRef<HTMLInputElement>(null);
 
-	const [isLoading, setIsLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [selectedVideo, setSelectedVideo] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-	const generateUploadUrl = useMutation(api.conversations.generateUploadUrl);
-	const sendImage = useMutation(api.messages.sendImage);
-	const sendVideo = useMutation(api.messages.sendVideo);
-	const me = useQuery(api.users.getMe);
+  const generateUploadUrl = useMutation(api.conversations.generateUploadUrl);
+  const sendImage = useMutation(api.messages.sendImage);
+  const sendVideo = useMutation(api.messages.sendVideo);
+  const me = useQuery(api.users.getMe);
+  const { selectedConversation } = useConversationStore();
 
-	const { selectedConversation } = useConversationStore();
+  const handleFileUpload = async (file: File, sendFunc: any) => {
+    try {
+      setIsLoading(true);
+      const postUrl = await generateUploadUrl();
+      const result = await fetch(postUrl, {
+        method: "POST",
+        headers: { "Content-Type": file.type },
+        body: file,
+      });
 
-	const handleSendImage = async () => {
-		setIsLoading(true);
-		try {
-			// Step 1: Get a short-lived upload URL
-			const postUrl = await generateUploadUrl();
-			// Step 2: POST the file to the URL
-			const result = await fetch(postUrl, {
-				method: "POST",
-				headers: { "Content-Type": selectedImage!.type },
-				body: selectedImage,
-			});
+      const { storageId } = await result.json();
+      await sendFunc({
+        conversation: selectedConversation!._id,
+        sender: me!._id,
+        imgId: storageId,
+      });
+    } catch (err) {
+      console.error("Failed to upload file", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-			const { storageId } = await result.json();
-			// Step 3: Save the newly allocated storage id to the database
-			await sendImage({
-				conversation: selectedConversation!._id,
-				imgId: storageId,
-				sender: me!._id,
-			});
+  return (
+    <>
+      <input
+        type="file"
+        ref={imageInput}
+        accept="image/*"
+        hidden
+        onChange={(e) => setSelectedImage(e.target.files![0])}
+      />
 
-			setSelectedImage(null);
-		} catch (error) {
-			console.error("Failed to send image");
-		} finally {
-			setIsLoading(false);
-		}
-	};
+      <input
+        type="file"
+        ref={videoInput}
+        accept="video/mp4"
+        hidden
+        onChange={(e) => setSelectedVideo(e.target.files![0])}
+      />
 
-	const handleSendVideo = async () => {
-		setIsLoading(true);
-		try {
-			const postUrl = await generateUploadUrl();
-			const result = await fetch(postUrl, {
-				method: "POST",
-				headers: { "Content-Type": selectedVideo!.type },
-				body: selectedVideo,
-			});
+      {selectedImage && (
+        <MediaDialog
+          isOpen={!!selectedImage}
+          onClose={() => setSelectedImage(null)}
+          file={selectedImage}
+          isLoading={isLoading}
+          handleSend={() => handleFileUpload(selectedImage, sendImage)}
+          mediaType="image"
+        />
+      )}
 
-			const { storageId } = await result.json();
+      {selectedVideo && (
+        <MediaDialog
+          isOpen={!!selectedVideo}
+          onClose={() => setSelectedVideo(null)}
+          file={selectedVideo}
+          isLoading={isLoading}
+          handleSend={() => handleFileUpload(selectedVideo, sendVideo)}
+          mediaType="video"
+        />
+      )}
 
-			await sendVideo({
-				videoId: storageId,
-				conversation: selectedConversation!._id,
-				sender: me!._id,
-			});
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Plus className="text-gray-600 dark:text-gray-400" />
+        </DropdownMenuTrigger>
 
-			setSelectedVideo(null);
-		} catch (err) {
-			console.log(err)
-		} finally {
-			setIsLoading(false);
-		}
-	};
-
-	return (
-		<>
-			<input
-				type='file'
-				ref={imageInput}
-				accept='image/*'
-				onChange={(e) => setSelectedImage(e.target.files![0])}
-				hidden
-			/>
-
-			<input
-				type='file'
-				ref={videoInput}
-				accept='video/mp4'
-				onChange={(e) => setSelectedVideo(e.target?.files![0])}
-				hidden
-			/>
-
-			{selectedImage && (
-				<MediaImageDialog
-					isOpen={selectedImage !== null}
-					onClose={() => setSelectedImage(null)}
-					selectedImage={selectedImage}
-					isLoading={isLoading}
-					handleSendImage={handleSendImage}
-				/>
-			)}
-
-			{selectedVideo && (
-				<MediaVideoDialog
-					isOpen={selectedVideo !== null}
-					onClose={() => setSelectedVideo(null)}
-					selectedVideo={selectedVideo}
-					isLoading={isLoading}
-					handleSendVideo={handleSendVideo}
-				/>
-			)}
-
-			<DropdownMenu>
-				<DropdownMenuTrigger>
-					<Plus className='text-gray-600 dark:text-gray-400' />
-				</DropdownMenuTrigger>
-
-				<DropdownMenuContent>
-					<DropdownMenuItem onClick={() => imageInput.current!.click()}>
-						<ImageIcon size={18} className='mr-1' /> Photo
-					</DropdownMenuItem>
-					<DropdownMenuItem onClick={() => videoInput.current!.click()}>
-						<Video size={20} className='mr-1' />
-						Video
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
-		</>
-	);
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => imageInput.current!.click()}>
+            <ImageIcon size={18} className="mr-1" /> Photo
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => videoInput.current!.click()}>
+            <Video size={20} className="mr-1" />
+            Video
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 };
+
 export default MediaDropdown;
 
-type MediaImageDialogProps = {
-	isOpen: boolean;
-	onClose: () => void;
-	selectedImage: File;
-	isLoading: boolean;
-	handleSendImage: () => void;
+type MediaDialogProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  file: File;
+  isLoading: boolean;
+  handleSend: () => void;
+  mediaType: "image" | "video";
 };
 
-const MediaImageDialog = ({ isOpen, onClose, selectedImage, isLoading, handleSendImage }: MediaImageDialogProps) => {
-	const [renderedImage, setRenderedImage] = useState<string | null>(null);
+const MediaDialog = ({
+  isOpen,
+  onClose,
+  file,
+  isLoading,
+  handleSend,
+  mediaType,
+}: MediaDialogProps) => {
+  const [renderedMedia, setRenderedMedia] = useState<string | null>(null);
 
-	useEffect(() => {
-		if (!selectedImage) return;
-		const reader = new FileReader();
-		reader.onload = (e) => setRenderedImage(e.target?.result as string);
-		reader.readAsDataURL(selectedImage);
-	}, [selectedImage]);
+  useEffect(() => {
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = (e) => setRenderedMedia(e.target?.result as string);
+    reader.readAsDataURL(file);
+  }, [file]);
 
-	return (
-		<Dialog
-			open={isOpen}
-			onOpenChange={(isOpen) => {
-				if (!isOpen) onClose();
-			}}
-		>
-			<DialogContent>
-				<DialogDescription className='flex flex-col gap-10 justify-center items-center'>
-					{renderedImage && <Image src={renderedImage} width={300} height={300} alt='selected image' />}
-					<Button className='w-full' disabled={isLoading} onClick={handleSendImage}>
-						{isLoading ? "Sending..." : "Send"}
-					</Button>
-				</DialogDescription>
-			</DialogContent>
-		</Dialog>
-	);
-};
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(isOpen) => {
+        if (!isOpen) onClose();
+      }}
+    >
+      <DialogContent>
+        <DialogDescription className="flex flex-col gap-10 justify-center items-center">
+          {mediaType === "image" && renderedMedia && (
+            <Image src={renderedMedia} width={300} height={300} alt="Media" />
+          )}
 
-type MediaVideoDialogProps = {
-	isOpen: boolean;
-	onClose: () => void;
-	selectedVideo: File;
-	isLoading: boolean;
-	handleSendVideo: () => void;
-};
+          {mediaType === "video" && renderedMedia && (
+            <ReactPlayer url={renderedMedia} controls width="100%" />
+          )}
 
-const MediaVideoDialog = ({ isOpen, onClose, selectedVideo, isLoading, handleSendVideo }: MediaVideoDialogProps) => {
-	const renderedVideo = URL.createObjectURL(new Blob([selectedVideo], { type: "video/mp4" }));
-
-	return (
-		<Dialog
-			open={isOpen}
-			onOpenChange={(isOpen) => {
-				if (!isOpen) onClose();
-			}}
-		>
-			<DialogContent>
-				<DialogDescription>Video</DialogDescription>
-				<div className='w-full'>
-					{renderedVideo && <ReactPlayer url={renderedVideo} controls width='100%' />}
-				</div>
-				<Button className='w-full' disabled={isLoading} onClick={handleSendVideo}>
-					{isLoading ? "Sending..." : "Send"}
-				</Button>
-			</DialogContent>
-		</Dialog>
-	);
+          <Button className="w-full" disabled={isLoading} onClick={handleSend}>
+            {isLoading ? "Sending..." : "Send"}
+          </Button>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
 };
